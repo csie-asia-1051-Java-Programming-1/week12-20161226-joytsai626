fdf3a5a87f84ca25c3ce109dac9e2c0b98794c0c
joytsai626 <105021013@live.asia.edu.tw>
2016-12-26 15:57:48 +0800

    ex2
    ex2

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,15 +1,23 @@
 ﻿package ex;
 /*
- * Topic: 給定一字串，請將此字串中，每個字元的 ASCII 編碼數值進行加總，輸出其編碼數 值總和。舉例來說，若輸入之字串為 “abcdefghij” 共 10 個字元，其 ASCII 編碼依序為 “97 98 99 100 101 102 103 104 105 106” ，則編碼總和輸出為 1015 。
+ * Topic: 給定一字串，請將此字串中，每個字元的 ASCII 編碼數值進行加總，輸出其編碼數 值總和。舉例來說，若輸入之字串為 “abcdefghij” 共 10 個字元，
+ * 其 ASCII 編碼依序為 “97 98 99 100 101 102 103 104 105 106” ，則編碼總和輸出為 1015 。
  * Date: 2016/12/26
- * Author: 1050210XX 周永振老師
+ * Author: 105021013
  */
-
+import java.util.*;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       Scanner scn = new Scanner(System.in); 
+       String b = scn.nextLine();
+       for(int i=0;i<0;i++);{
+       int sum = 0;   
+       for(int j=0;j<b.length();j++){
+    	   sum = sum + b.charAt(j);
+       }
+       System.out.println(sum);
 	}
-
 }
+}      
